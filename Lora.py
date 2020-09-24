7a98ab598f93c934794818feb665babfcab0bb9d
Zhe ming Wu <37643421+boy07132004@users.noreply.github.com>
2020-09-24 14:05:53 +0800

    Add init func

--- Lora.py
+++ Lora.py
@@ -1,7 +1,8 @@
 import serial
 import time
 class lora:
-    def __init__(self,port="/dev/ttyS0",baudrate=9600):
+    def __init__(self,port="/dev/ttyS0",baudrate=9600,init=False):
+        if init:self.init_lora_device(port)
         self.ser = serial.Serial(port,baudrate=baudrate,timeout=0.5)
         self.AT_command("AT+ADDRESS=6",time_sleep=0.2)
         self.AT_command("AT+PARAMETER=12,7,1,4",time_sleep=0.2)
@@ -15,17 +16,25 @@ class lora:
         self.ser.write(cmd.encode())
         time.sleep(time_sleep)
         print(f"{cmd.split('=')[0][3:]} > {self.ser.read(10).decode('UTF-8')}")
-        
-    def close(self):
-        self.ser.close()
     
     def send(self,message,address=713):
         length = len(str(message))
         self.AT_command(f"AT+SEND={address},{length},{message}\r\n")
+    
+    def init_lora_device(self,port):
+        s = serial.Serial(port,baudrate=115200)
+        s.write("AT+IPR=9600\r\n".encode())
+        print(f"init Done")
+        s.close()
+        time.sleep(1)
+    def close(self):
+        self.ser.close()
+    
+    
         
         
 if __name__ == "__main__" :
-    _lora = lora()
-    for _ in range(5): _lora.send("send to 713")
+    _lora = lora(init=True)
+    for _ in range(10): _lora.send("send to 713")
     _lora.close()
     
